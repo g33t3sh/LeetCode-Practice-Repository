8a249de833c6fa838fbd1ccd57b420c911543e0d
GEETESH KUMAR <info.geetesh@gmail.com>
2025-01-05 12:05:43 +0530

    Time: 0 ms (100%), Space: 42.3 MB (7.12%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Solution { 
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1; 
+        return newNumber; 
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution(); 
+        int[] digits = {9, 9, 9}; 
+        int[] result = solution.plusOne(digits);
+        System.out.println(Arrays.toString(result)); 
+    }
+}
